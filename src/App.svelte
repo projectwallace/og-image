aeadb9e9e277cb4f825f52f69cd0f293ccbce855
Bart Veneman <bartveneman@users.noreply.github.com>
2020-03-15 23:35:48 +0100

    include Svelte generator page

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Input from "./Input.svelte";
+  import Select from "./Select.svelte";
+  import RequestUrl from "./RequestUrl.svelte";
+
+  const card = {
+    text: "Beautiful **CSS Analytics** in your Terminal.",
+    icon: "wallace-cli",
+    cli: "npx wallace-cli css-analytics.com"
+  };
+</script>
+
+<div class="bg-gray-200 py-8 xl:flex antialiased h-full">
+  <div class="px-8 max-w-2xl">
+    <div class="mb-8">
+      <Input
+        label="Title"
+        value={card.text}
+        on:input={event => (card.text = event.target.value)} />
+      <Input
+        label="CLI Input"
+        value={card.cli}
+        on:input={event => (card.cli = event.target.value)} />
+      <Select
+        label="Icon"
+        value={card.icon}
+        on:change={event => (card.icon = event.target.value)} />
+    </div>
+  </div>
+
+  <div class="overflow-auto mt-16 px-8 xl:mt-0">
+    <RequestUrl {...card} />
+  </div>
+</div>
---
+++ src/Input.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  export let value;
+  export let label;
+</script>
+
+<style>
+  .row {
+    display: grid;
+    grid-template-columns: 10rem 1fr;
+    align-items: center;
+  }
+</style>
+
+<label class="row mb-3">
+  <span>{label}</span>
+  <input
+    class="bg-white border-gray-400 rounded py-2 px-3"
+    bind:value
+    type="text"
+    on:input />
+</label>
